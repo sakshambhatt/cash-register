66007b155cc2e2e16b1d4886cc704fc16c19d7ad
Saksham Bhatt <sakshambhatt33@gmail.com>
2021-01-04 23:36:44 +0530

    style; reuse fixed

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var nextButton = document.querySelector("#next-btn");
 var button = document.querySelector("#btn");
 var output = document.querySelector("#output");
 var change = document.querySelector("#change");
-var notes = [2000, 500, 100, 20, 10, 5, 1];
-var billNumber = [0, 0, 0, 0, 0, 0, 0];
+//array of all denominations
+var notes = [2000, 500, 100, 20, 10, 5, 1]; 
+//array giving the number of bills/ notes of each denominations
+var billNumber = [0, 0, 0, 0, 0, 0, 0]; 
+//array of elements related to output
 var outputArray = [
     document.querySelector("#n2000"), 
     document.querySelector("#n500"),
@@ -18,6 +21,8 @@ var outputArray = [
 var billAmount = -1;
 var cashAmount = -1;
 
+//buffer is difference between bill-amount and cash-given
+//function to calculate change with min number of notes 
 function balanceCalculator (buffer) {
     
     for(var i = 0; i < notes.length; i++) {
@@ -27,12 +32,16 @@ function balanceCalculator (buffer) {
           billNumber[i] = parseInt(buffer / notes[i]);
           buffer = buffer - (notes[i] * parseInt(buffer / notes[i]));
         }
+        else {
+            billNumber[i] = 0;
+        }
     }
     for(var i = 0; i < outputArray.length; i++) {
 
         outputArray[i].innerText = billNumber[i];
     }
 }
+
 
 nextButton.addEventListener('click', function billAmountChangeHandler(e) {
    
@@ -48,21 +57,28 @@ nextButton.addEventListener('click', function billAmountChangeHandler(e) {
 });
 button.addEventListener("click", function btnClickHandler() {
 
+    //redundancy if the user decides to run the app without reloading
+    billAmount = parseInt(billInput.value);
+
     cashAmount = parseInt(cashInput.value);
    
     if(cashAmount < 0) {
-
         output.innerText = "cash amount can't be negative";
-    } else {
+    } else if(billAmount < 0) {
+        output.innerText = "bill amount can't be negative";
+    } else if(cashAmount < 0 && billAmount < 0) {
+        output.innerText = "bill and cash amounts can't be negative";
+    }
+    else {
         if(cashAmount > billAmount) {
             
-            output.innerText = "Give like this to minimize number of notes";
+            output.innerText = "give change like this to minimize number of notes";
             var buffer = cashAmount - billAmount;
             balanceCalculator(buffer);
         }
          else if(billAmount > cashAmount) {
 
-            output.innerText = "Take more to clear the balance...";
+            output.innerText = "take more to clear the balance...";
             var buffer = billAmount - cashAmount;
             balanceCalculator(buffer);
         } else if(billAmount === cashAmount) {
